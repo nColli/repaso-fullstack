03e1501dd2825d55740a2960569611d56c6b204e
ncolli <collinicolas01@gmail.com>
2025-03-31 08:16:04 -0300

    handling arrays and async state updates

--- part1/practice-part1-d/src/App.jsx
+++ part1/practice-part1-d/src/App.jsx
@@ -5,6 +5,8 @@ const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
 const App = () => {
   const [ left, setLeft ] = useState(0)
   const [ right, setRight ] = useState(0)
+  const [ allClicks, setAllClicks ] = useState([]) 
+  const [ total, setTotal ] = useState(0)
 
   //guardado en forma de objeto
   /*
@@ -32,20 +34,33 @@ const App = () => {
   */
   const handleLeft = () => {
     console.log('handling left...');
-    setLeft(left + 1)
+    setAllClicks(allClicks.concat('L')) //concat no muta el array allClciks, devuelve una copia con el elem agregado
+    //como el estado de los componentes no se debe modificar directamente, no uso push
+    
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    //setTotal(left + right) <- no funciona xq la actualización del estado es asincrona, se renderiza en algun momento antes de que el componnete se renderecide de nuevo
+    setTotal(updatedLeft + right)
   }
 
   const handleRight = () => {
     console.log('handling right...');
-    setRight(right + 1)
+    setAllClicks(allClicks.concat('R'))
+
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    //setTotal(right + left)
+    setTotal(updatedRight + left)
   }
   
   return (
     <div>
-      <h4>{left}</h4>
+      <h3>{left}</h3>
       <Button text='left' onClick={handleLeft} />
       <Button text='right' onClick={handleRight} />
-      <h4>{right}</h4>
+      <h3>{right}</h3>
+      <h3>{allClicks.join(' ')}</h3>
+      <h2>Total: {total}</h2>
     </div>
   )
 }
