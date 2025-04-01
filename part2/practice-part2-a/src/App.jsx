dc358be6b019b4af842c94127afbbabdab591ed1
ncolli <collinicolas01@gmail.com>
2025-03-31 23:05:17 -0300

    map pratice-part2-a

---
+++ part2/practice-part2-a/src/App.jsx
@@ -0,0 +1,24 @@
+const Note = ({ note }) => {
+  return (
+    <li>{note.content}</li>
+  )
+}
+
+const App = ({ notes }) => {
+
+  const result = notes.map(note => note.id) //return array of objects [ {0: 1}, {1: 2}, {2: 3} ]
+  console.log(result);
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
