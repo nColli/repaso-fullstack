d05eef033748e89ef32c9ba3b2e9529a0f752379
ncolli <collinicolas01@gmail.com>
2025-04-02 10:17:38 -0300

    PUT request, change importance of note

--- part2/practice-part2-d/src/App.jsx
+++ part2/practice-part2-d/src/App.jsx
@@ -40,6 +40,17 @@ const App = () => {
     ? notes
     : notes.filter( (note) => note.important )
   
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important}
+
+    axios
+      .put(url, changedNote)
+      .then(response => {
+        setNotes(notes.map(note => note.id !== id ? note: response.data))
+      })
+  }
 
   return (
     <div>
@@ -51,7 +62,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note 
+            key={note.id} 
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)} 
+          />
         )}
       </ul>
       <form onSubmit={addNote}>
